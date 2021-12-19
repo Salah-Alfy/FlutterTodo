2cc3b62b591a84b4d191ddf4a60e8cd67c188aef
Salah-Alfy <salah.alfy30@gmail.com>
2021-12-20 00:31:12 +0200

    Default Dialog

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: unnecessary_overrides
+
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:todos/app/data/models/task.dart';
 import 'package:todos/app/data/services/storage/task_repository.dart';
@@ -7,17 +10,36 @@ class HomeController extends GetxController {
 
   HomeController({required this.taskRepository});
 
+  final formKey = GlobalKey<FormState>();
+  final editController = TextEditingController();
+  final chipIndex = 0.obs;
   final tasks = <Task>[].obs;
 
   @override
   void onInit() {
     super.onInit();
     tasks.assignAll(taskRepository.readTask());
-    ever(tasks, (_) => taskRepository.writeTasks(tasks));//when ever task changes write task
+    ever(
+        tasks,
+        (_) => taskRepository
+            .writeTasks(tasks)); //when ever task changes write task
   }
 
   @override
   void onClose() {
     super.onClose();
+  }
+
+  void changeChipIndex(int value) {
+    chipIndex.value = value;
+  }
+
+  bool addTask(Task task) {
+    if (tasks.contains(task)) {
+      //here equtable compare if task have same icon name color then no add 
+      return false;
+    }
+    tasks.add(task);
+    return true;
   }
 }
--- lib/app/modules/home/widgets/add_card.dart
+++ lib/app/modules/home/widgets/add_card.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: must_be_immutable, prefer_typing_uninitialized_variables
+
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:todos/app/core/utils/extensions.dart';
+import 'package:todos/app/core/values/colors.dart';
+import 'package:todos/app/data/models/task.dart';
 import 'package:todos/app/modules/home/controller.dart';
 import 'package:todos/app/widgets/icons.dart';
 
@@ -21,7 +26,89 @@ class AddCard extends StatelessWidget {
         height: squareWidth / 2,
         margin: EdgeInsets.all(3.0.wp),
         child: InkWell(
-          onTap: () {},
+          onTap: () {
+            Get.defaultDialog(
+                titlePadding: EdgeInsets.symmetric(vertical: 5.0.wp),
+                radius: 5,
+                title: "Task Type",
+                content: Form(
+                    key: homeController.formKey,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 3.0.wp),
+                          child: TextFormField(
+                            controller: homeController.editController,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "title"),
+                            validator: (value) {
+                              if (value == null || value.trim().isEmpty) {
+                                return "Please Enter Your Task Title";
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 5.0.wp),
+                          child: Wrap(
+                              spacing: 2.0.wp,
+                              children: icons
+                                  .map((e) => Obx(() {
+                                        final index = icons.indexOf(e);
+                                        return ChoiceChip(
+                                          label: e,
+                                          selectedColor: Colors.grey[200],
+                                          pressElevation: 0,
+                                          backgroundColor: Colors.white,
+                                          selected:
+                                              homeController.chipIndex.value ==
+                                                  index,
+                                          onSelected: (bool selected) {
+                                            homeController.chipIndex.value =
+                                                selected ? index : 0;
+                                          },
+                                        );
+                                      }))
+                                  .toList()),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (homeController.formKey.currentState!
+                                .validate()) {
+                              int icon = icons[homeController.chipIndex.value]
+                                  .icon!
+                                  .codePoint;
+                              String color =
+                                  icons[homeController.chipIndex.value]
+                                      .color!
+                                      .toHex();
+                              var task = Task(
+                                  title: homeController.editController.text,
+                                  icon: icon,
+                                  color: color);
+                              Get.back();
+                              homeController.addTask(task)
+                                  ? EasyLoading.showSuccess("Create Sucess")
+                                  : EasyLoading.showError("Duplicated Task");
+                            }
+                          },
+                          child: const Text("Confirm"),
+                          style: ElevatedButton.styleFrom(
+                            primary: blue,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            minimumSize: const Size(150, 40),
+                          ),
+                        )
+                      ],
+                    )));
+
+            homeController.editController.clear();
+            homeController.changeChipIndex(0);
+          },
           child: DottedBorder(
             color: Colors.grey[400]!,
             dashPattern: const [8, 4],
